456c011a64568377f61247fd0a750196ab2fe3ff
Njabulo <njabulo@xcontent.com>
2022-02-16 15:53:16 +0200

    assignemnt 2 practice , using two way binding

--- my-first-app/src/app/server/server.component.ts
+++ my-first-app/src/app/server/server.component.ts
@@ -5,5 +5,15 @@ import { Component } from "@angular/core";
     templateUrl:'./server.component.html'
 })
 export class ServerComponent{
-
+    //assignment2
+    username='';
+    //variable with a type infered
+    serverId=10;
+    //variable with a type explicitly declared
+    serverStatus : string ="offline";
+    // methods being called from the template need to return strings
+    getServerStatus()
+    {
+        return this.serverStatus;
+    }
 }
--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -6,10 +6,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  allowNewServer=false;
+  serverCreationStatus ='No server created';
+  serverName='default';
+  username='';
+  isUserNameEmpty=true;
 
-  constructor() { }
+
+  constructor() {
+    setTimeout(()=>{
+      this.allowNewServer=true;
+    },2000);
+   }
 
   ngOnInit(): void {
+  }
+
+  onCreateServer(){
+    this.serverCreationStatus='Server was created';
+  }
+
+  onUpdateServerName(event: Event){
+    this.serverName=(<HTMLInputElement>event.target).value;
+  }
+
+  resetUserName(){
+    this.username='';
+    this.isUserNameEmpty=true;
+  }
+
+  onKey(event: any){
+    this.isUserNameEmpty=false;
   }
 
 }
